02a7e44143ece46b990cc06978d160fdc269f998
Aliaksei Poliak <aliakseipv@gmail.com>
2024-02-20 17:27:33 -0500

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { store } from './store/store.js'
 import { Provider, } from 'react-redux'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Provider store={store}>
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,5 @@ export default defineConfig({
 	plugins: [react()],
 	define: {
 		'process.env': process.env
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
+	}
 })
