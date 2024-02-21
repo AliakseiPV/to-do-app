d986190ee78efd92f9cdda9f4fb8c4900f6c2294
Aliaksei Poliak <aliakseipv@gmail.com>
2024-02-21 11:43:15 -0500

    change default route

--- client/src/store/api/api.js
+++ client/src/store/api/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const api = createApi({
-	reducerPath: 'api',
+	reducerPath: 'aoi',
 	tagTypes: ['User', 'List', 'Todo'],
 	baseQuery: fetchBaseQuery({
 		baseUrl: import.meta.env.VITE_API_URL,
--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use('/api', router)
+app.use('/', router)
 
 
 app.use(errorHandler)
