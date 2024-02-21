a804aaa6190b122429a28bc675e206bb7cfaeeb8
Aliaksei Poliak <aliakseipv@gmail.com>
2024-02-21 12:28:19 -0500

    change index.js

--- server/index.js
+++ server/index.js
@@ -3,19 +3,26 @@ const express = require('express')
 const cors = require('cors')
 const sequelize = require('./db')
 const router = require('./routes/index')
-const PORT = process.env.PORT || 5000 
+const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser')
 const errorHandler = require('./middlware/ErrorHandlingMiddleware')
 
 
 const app = express()
 
+app.use(function (req, res, next) {
+	res.setHeader(
+		'Content-Security-Policy-Report-Only', "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
+	)
+	next()
+})
 app.use(cors({
 	origin: 'https://to-do-app-client.onrender.com',
 	methods: ['GET', 'POST', 'DELETE', 'PUT'],
 }))
+
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api', router)
 
 
@@ -27,7 +34,7 @@ const start = async () => {
 		await sequelize.sync()
 		app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
 	} catch (error) {
-		
+
 	}
 }
 
